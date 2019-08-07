d5db648cff6c5142d14cbe5d0e354711d97343b8
Darius Mejeras <damejeras@gmail.com>
2019-08-07 16:44:22 +0300

    Another test

--- sentinel.go
+++ sentinel.go
@@ -145,7 +145,6 @@ func (sc *Client) doOnce(cmd string, args ...interface{}) (interface{}, error) {
 func (sc *Client) MasterAddress(name string) (string, error) {
 	sc.Lock()
 	defer sc.Unlock()
-	_, err := sc.do("AUTH", "user:pa55w0rd")
 	res, err := redis.Strings(sc.do("SENTINEL", "get-master-addr-by-name", name))
 	masterAddr := strings.Join(res, ":")
 	return masterAddr, err
